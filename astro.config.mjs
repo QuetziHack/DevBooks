a21cf68dd4a62a6361d751c29ca69c503e1ade02
QuetziHack <quetzalion.de.fuego@gmail.com>
2025-06-15 18:29:08 -0600

    workflow creado

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import tailwind from '@astrojs/tailwind';
 
 // SOLUCIÓN PARA TAILWIND v3
 export default defineConfig({
+  site: 'https://QuetziHack.github.io',
+  base: 'DevBooks',
   integrations: [tailwind()]
 });
