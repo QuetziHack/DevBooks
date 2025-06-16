1b063c5fbb929e3d26b46a0ab710f775a6e20da6
QuetziHack <quetzalion.de.fuego@gmail.com>
2025-06-15 19:16:32 -0600

    dominio issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 
 // SOLUCIÓN PARA TAILWIND v3
 export default defineConfig({
-
+  site: 'https://QuetziHack.github.io',
   base: 'https://devbooks.n8nintegration.online',
   integrations: [tailwind()]
 });
