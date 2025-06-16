4d63397b1ed3c184a0e4a41e9970b827aab779a6
QuetziHack <quetzalion.de.fuego@gmail.com>
2025-06-15 19:32:29 -0600

    dominio issues 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,5 @@ import cname from 'vite-plugin-cname';
 export default defineConfig({
   site: 'https://devbooks.n8nintegration.online',
   base: '/',
-  integrations: [tailwind()],
-  vite: {
-    plugins: [cname({ domain: 'devbooks.n8nintegration.online' })]
-  }
+  integrations: [tailwind()]
 });
